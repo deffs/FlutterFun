f3234344af702def7029544131d9f47fb4fe7960
Alex de France <defrance@umich.edu>
2018-05-17 15:43:01 -0400

    bmi app done

--- bmi_app/lib/ui.dart
+++ bmi_app/lib/ui.dart
@@ -9,6 +9,37 @@ class BMI extends StatefulWidget {
 }
 
 class BMIState extends State<BMI> {
+  var _ageCont = new TextEditingController();
+  var _heightCont = new TextEditingController();
+  var _weightCont = new TextEditingController();
+  String _myString = "";
+  String _resultStr = "";
+  var BMI = 0.0;
+  calcBMI() {
+    FocusScope.of(context).requestFocus(new FocusNode());
+    setState(() {
+      if (_heightCont.text.isNotEmpty && _weightCont.text.isNotEmpty) {
+        BMI = (double.parse(_weightCont.text) /
+            ((double.parse(_heightCont.text) * 12) *
+                (double.parse(_heightCont.text) * 12))) * 703;
+        _myString = "Your BMI: ${BMI.toStringAsFixed(1)}";
+        if (BMI < 18.5) {
+          _resultStr = "Underweight";
+        }
+        else if (BMI >= 18.5 && BMI < 25.0) {
+          _resultStr = "Normal";
+        }
+        else if (BMI >= 25.0 && BMI < 30.0) {
+          _resultStr = "Overweight";
+        } else {
+          _resultStr = "Obese";
+        }
+      } else {
+        _myString = "Please Enter Your Details!";
+        _resultStr = "";
+      }
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -20,7 +51,7 @@ class BMIState extends State<BMI> {
           color: Colors.grey.shade100,
         ),
         ),
-        backgroundColor: Colors.pinkAccent,
+        backgroundColor: Colors.purpleAccent,
         centerTitle: true,
       ),
       body: new Container(
@@ -41,25 +72,28 @@ class BMIState extends State<BMI> {
               child: new Column(
                 children: <Widget>[
                   new TextField(
-                    controller: null,
+                    controller: _ageCont,
                     decoration: new InputDecoration(
                       icon: Icon(Icons.person),
                       labelText: "Age",
                     ),
+                    keyboardType: TextInputType.number,
                   ),
                   new TextField(
-                    controller: null,
+                    controller: _heightCont,
                     decoration: new InputDecoration(
                       icon: Icon(Icons.insert_chart),
                       labelText: "Height in feet.inches",
                     ),
+                    keyboardType: TextInputType.number,
                   ),
                   new TextField(
-                    controller: null,
+                    controller: _weightCont,
                   decoration: new InputDecoration(
                     icon: Icon(Icons.line_weight),
                     labelText: "Weight in lbs",
                   ),
+                    keyboardType: TextInputType.number,
                   ),
                   new Padding(padding: new EdgeInsets.only(top: 16.0)),
                   new CupertinoButton(
@@ -71,14 +105,37 @@ class BMIState extends State<BMI> {
                           fontSize: 17.0,
                         ),
                       ), 
-                      onPressed: () => debugPrint("calc"),
-                    color: Colors.pinkAccent,
+                      onPressed: calcBMI,
+                    color: Colors.purpleAccent,
                     borderRadius: new BorderRadius.all(new Radius.circular(3.0)),
-                    padding: new EdgeInsets.all(16.0),
-                  )
+                    padding: new EdgeInsets.all(12.0),
+                  ),
                 ],
               ),
-            )
+            ),
+            new Container(
+              alignment: Alignment.center,
+              padding: new EdgeInsets.all(16.0),
+              child: new Column(
+                children: <Widget>[
+                  new Text("$_myString",
+                    style: new TextStyle(
+                        fontSize: 17.0,
+                        color: Colors.yellowAccent,
+                        fontWeight: FontWeight.w400
+                    ),
+                  ),
+                  new Padding(padding: new EdgeInsets.all(6.0)),
+                  new Text("$_resultStr",
+                    style: new TextStyle(
+                        fontSize: 17.0,
+                        color: Colors.purpleAccent.shade700,
+                        fontWeight: FontWeight.w400
+                    ),
+                  ),
+                ],
+              )
+            ),
           ],
         ),
       ),
